b91473725af9bc2ebd92ebbe829c1c4ce9e73ec7
LIGUANYI <74758995+leeGuanyi@users.noreply.github.com>
2021-03-23 15:06:16 +0800

    Update Default.aspx.cs

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,33 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+           int year = 1000;
 
+            bool isleap = false;
+
+            if (year % 4 == 0)
+            {
+                isleap = true;
+
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        isleap = true;
+                    }
+                    else
+                    {
+                        isleap = false;
+                    }
+                }
+            }
+            if (isleap == true)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
         }
     }
 }
